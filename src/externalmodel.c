4f3e8cd5317d5f06f7c2f58c3af8db359e25b2ac
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2022-08-22 12:37:03 +0100

    Tweak on example model for name string

--- src/externalmodel.c
+++ src/externalmodel.c
@@ -6,6 +6,7 @@
 /*------------------------------------------------------------------------------------------------*/
 
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
 
 #ifdef __cplusplus
@@ -13,16 +14,20 @@ extern "C" {
 #endif
 
   typedef struct {
-    /* Declared capabilities of this model for the DFTB+ code */
+    /* Declared capabilities of this model for initial setup of the
+       DFTB+ code */
 
     // Can this library deliver a hamiltonian?
     bool hamiltonian;
 
-    // Can this library deliver an overlap matrix (i.e. non-orthogonal)?
+    // Can this library deliver an overlap matrix (i.e. is a non-orthogonal model)?
     bool overlap;
 
-    // Does this library deliver energy terms other than the band-strucuture energy?
+    // Does this library deliver energy terms (other than the band-structure energy)?
     bool energy;
+
+    // Is the model self-consistent
+    bool selfconsistent;
 
     // Can model results be returned for a subset of atoms?
     bool atomsubset;
@@ -33,14 +38,20 @@ extern "C" {
   } mycapabilities;
 
 
-  void dftbp_provided_with(typeof (mycapabilities) *capabilities){
+  void dftbp_provided_with(char* modelname, typeof (mycapabilities) *capabilities){
 
     /* Declare capabilities of this model to DFTB+ via the API */
 
+    // Name of external model
+    sprintf(modelname, "Huckel toy model");
+
+    // flags for capabilities
     *capabilities = (mycapabilities) {
-      .hamiltonian=false, .overlap = true, .energy = false, .atomsubset = false, .mpi = false
+      .hamiltonian=true, .overlap = false, .energy = false, .selfconsistent = false,
+      .atomsubset = false, .mpi = false
     };
 
+    return;
   }
 
 #ifdef __cplusplus
