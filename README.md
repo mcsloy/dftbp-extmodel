627736e737454d8be47bb33ec4ce20141205fd72
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2022-08-28 15:25:06 +0100

    Start of documentation

---
+++ README.md
@@ -0,0 +1,10 @@
+External models for (semi-)empirical chemistry
+==============================================
+
+[![image](https://gitlicense.com/badge/bhourahine/dftbp_extmodel)](https://gitlicense.com/license/bhourahine/dftbp_extmodel)
+
+Externally provides a model to the semiempirical code
+[DFTB+](https://www.dftbplus.org) via a library structure.
+
+An example of a simple model (Hückel-like) is given and documentation
+for adding other external models.
--- src/externalmodel.c
+++ src/externalmodel.c
@@ -1,9 +1,13 @@
-/*------------------------------------------------------------------------------------------------*/
-/*  Example external model for the DFTB+ project: www.dftbplus.org                                */
-/*  Copyright (C) 2006 - 2022  B. Hourahine                                                       */
-/*                                                                                                */
-/*  See the LICENSE file for terms of usage and distribution.                                     */
-/*------------------------------------------------------------------------------------------------*/
+/**
+
+@brief
+Example external model for the DFTB+ project: www.dftbplus.org
+Copyright (C) 2006 - 2022  B. Hourahine
+
+See the LICENSE file for terms of usage and distribution.
+
+@file
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,56 +18,94 @@
 extern "C" {
 #endif
 
+  /** Declared capabilities of this model for initial setup of the
+      DFTB+ code */
   typedef struct {
-    /* Declared capabilities of this model for initial setup of the
-       DFTB+ code */
 
-    // Can this library deliver a hamiltonian?
+    /** Can this library deliver a hamiltonian to the calling code? */
     bool hamiltonian;
 
-    // Can this library deliver an overlap matrix (i.e. is a non-orthogonal model)?
+    /** Can this library deliver an overlap matrix (i.e. is a non-orthogonal model)? */
     bool overlap;
 
-    // Does this library deliver energy terms (other than the band-structure energy)?
+    /** Does this library deliver energy terms (other than the band-structure energy)? */
     bool energy;
 
-    // Is the model self-consistent
+    /** Is the model self-consistent */
     bool selfconsistent;
 
-    // Can model results be returned for a subset of atoms?
+    /** Number of spin channels supported by model (none:0, collinear:1, non-collinear:3) */
+    int spinchannels;
+
+    /** Can model results be returned for a subset of atoms? */
     bool atomsubset;
 
-    // Is this library MPI aware (so requires a communicator)?
+    /** Is this library MPI aware (so requires a communicator)? */
     bool mpi;
 
   } mycapabilities;
 
 
+  /** Internal state of this model, including any initialisation
+      passed in from DFTB+ */
   typedef struct {
-    /* Internal state of this model, including any initialisation from DFTB+ */
 
+    
   } mystate;
 
 
-  void dftbp_provided_with(char* modelname, typeof (mycapabilities) *capabilities){
+  /**
+     Combined application programming interface and application binary
+     interface semantic version, as supported by this library. Note,
+     the format and is externally defined in the DFTB+ project
 
-    /* Declare capabilities of this model to DFTB+ via the API */
+     @param major Major version, revised on breaking changes
+     @param minor Minor version, revised on extensions
+     @param patch Patch version, revised on invisible changes
+
+  */
+  void dftbp_model_apbi(int* major, int* minor, int* patch){
+    *major = 0;
+    *minor = 1;
+    *patch = 0;
+  }
+
+
+  /**
+     Declare capabilities of this model to DFTB+ via the external model API.
+
+     @param modelname null terminated string for name of this model
+     @param capabilities structure with capabilities of the model
+
+   */
+  void dftbp_provided_with(char* modelname, typeof (mycapabilities) *capabilities){
 
     // Name of external model
     sprintf(modelname, "Huckel toy model");
 
-    // flags for capabilities
+    // flags for capabilities of this specific model
     *capabilities = (mycapabilities) {
       .hamiltonian = true, .overlap = false, .energy = false, .selfconsistent = false,
-      .atomsubset = false, .mpi = false
+      .spinchannels = 0, .atomsubset = false, .mpi = false
     };
 
     return;
   }
 
+  /**
+     Set up this model, read some settings from DFTB+ over it's
+     external model API, read a parameter file and initialise it's
+     data structure for handling via DFTB+.
 
+     @param nspecies number of chemical species/types present
+     @param species array of null terminated strings labelling chemical species
+     @param message return message, in event of routine failure (return != 0)
+
+     @return 0 on successful return, non-zero if there is an error
+     message to check
+
+   */
   int initialise_model_for_dftbp(int* nspecies, char* species[], char* message) {
-    /* Set up model, read some settings from DFTB+, read a parameter file */
 
     FILE *input;
     int i;
@@ -76,6 +118,7 @@ extern "C" {
       return -1;
     }
 
+    // This specific model is only for H and C atoms, so will throw an error otherwise
     for (i=0;i<*nspecies;i++) {
       if (strcmp(species[i], "C") != 0 && strcmp(species[i], "H") != 0) {
 	sprintf(message, "Toy library only knows about C and H atoms, not %s.\n", species[i]);
@@ -83,11 +126,12 @@ extern "C" {
       }
     }
 
-    // blank message if nothing happening
+    // blank return message if nothing happening
     sprintf(message, "\n");
     return 0;
 
   }
+
 
 #ifdef __cplusplus
 }
