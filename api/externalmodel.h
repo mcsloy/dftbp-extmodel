08c7c9afa3c6e1a68c7c20dd7c811e69661fcb08
Ben Hourahine <benjamin.hourahine@strath.ac.uk>
2022-09-03 18:43:59 +0100

    Header file

---
+++ api/externalmodel.h
@@ -0,0 +1,136 @@
+/**
+
+   @brief
+   Headers for example external model for the DFTB+ project:
+   www.dftbplus.org Copyright (C) 2022 B. Hourahine
+
+   See the LICENSE file for terms of usage and distribution.
+
+   @file
+*/
+
+#ifndef H_EXTERNALMODEL
+#define H_EXTERNALMODEL
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  /** Declared capabilities of this model for initial setup of the
+      DFTB+ code */
+  typedef struct {
+
+    /** Can this library deliver a hamiltonian to the calling code? */
+    bool hamiltonian;
+
+    /** Can this library deliver an overlap matrix (i.e. is a non-orthogonal model)? */
+    bool overlap;
+
+    /** Does this library deliver energy terms (other than the band-structure energy)? */
+    bool energy;
+
+    /** Is the model self-consistent */
+    bool selfconsistent;
+
+    /** Number of spin channels supported by model (none:0, collinear:1, non-collinear:3) */
+    int spinchannels;
+
+    /** Can model results be returned for a subset of atoms? */
+    bool atomsubset;
+
+    /** Is this library MPI aware (so requires a communicator)? */
+    bool mpi;
+
+  } mycapabilities;
+
+
+  /** Internal state of this model, including any initialisation passed
+      in from DFTB+, any data read in, checks for state, etc. */
+  typedef struct {
+
+    bool initialised;
+    int number;
+
+  } mystate;
+
+
+  /**
+     Combined application programming interface and application binary
+     interface semantic version, as supported by this library. Note,
+     the format and is externally defined in the DFTB+ project
+
+     @param major Major version, revised on breaking changes
+     @param minor Minor version, revised on extensions
+     @param patch Patch version, revised on invisible changes
+
+  */
+  void dftbp_model_apbi(int* major, int* minor, int* patch);
+
+  /**
+     Declare capabilities of this model to DFTB+ via the external model API.
+
+     @param modelname null terminated string for name of this model
+     @param capabilities structure with capabilities of the model
+
+  */
+  void dftbp_provided_with(char* modelname, typeof (mycapabilities) *capabilities);
+
+  /**
+     Set up this model, read some settings from DFTB+ over it's
+     external model API and initialise it's data structure for
+     handling via DFTB+.
+
+     @param nspecies number of chemical species/types present
+     @param species array of null terminated strings labelling chemical species
+     @param cutoffs array of cutoffs for distance over which atoms of
+     each species have hamiltonian interactions
+     @param state internal state and data of the model, not checked in
+     DFTB+, just passed around
+     @param message return message, in event of routine failure (return != 0)
+
+     @return 0 on successful return, non-zero if there is an error
+     message to check
+
+   */
+  int initialise_model_for_dftbp(int* nspecies, char* species[], double cutoffs[], typeof (mystate) *state,
+                                 char* message);
+
+
+    /**
+     Update this model, using geometric and other information from
+     DFTB+ over it's external model API.
+
+     @param state internal state and data of the model, this is not
+     checke by DFTB+, just passed around by it
+
+     @param message return message, in event of routine failure
+     (return != 0)
+
+     @return 0 on successful return, non-zero if there is an error
+     message to check
+
+   */
+  int update_model_for_dftbp(typeof (mystate) *state, char* message);
+
+
+  /**
+     Clean up after this model, freeing any memory in the mystate type
+
+     @param state internal state and data of the model. This is not
+     checke by DFTB+, just passed around by it, so we need to remove
+     any allocated memory here.
+
+     @param message return message, in event of routine failure
+     (return != 0)
+
+     @return 0 on successful return, non-zero if there is an error
+     message to check
+
+  */
+  int cleanup_model_for_dftbp(typeof (mystate) *state, char* message);
+  
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- src/externalmodel.c
+++ src/externalmodel.c
@@ -2,7 +2,7 @@
 
 @brief
 Example external model for the DFTB+ project: www.dftbplus.org
-Copyright (C) 2006 - 2022 B. Hourahine
+Copyright (C) 2022 B. Hourahine
 
 See the LICENSE file for terms of usage and distribution.
 
@@ -13,194 +13,151 @@ See the LICENSE file for terms of usage and distribution.
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-  /** Declared capabilities of this model for initial setup of the
-      DFTB+ code */
-  typedef struct {
-
-    /** Can this library deliver a hamiltonian to the calling code? */
-    bool hamiltonian;
-
-    /** Can this library deliver an overlap matrix (i.e. is a non-orthogonal model)? */
-    bool overlap;
-
-    /** Does this library deliver energy terms (other than the band-structure energy)? */
-    bool energy;
-
-    /** Is the model self-consistent */
-    bool selfconsistent;
-
-    /** Number of spin channels supported by model (none:0, collinear:1, non-collinear:3) */
-    int spinchannels;
-
-    /** Can model results be returned for a subset of atoms? */
-    bool atomsubset;
-
-    /** Is this library MPI aware (so requires a communicator)? */
-    bool mpi;
-
-  } mycapabilities;
+#include "../api/externalmodel.h"
 
 
-  /** Internal state of this model, including any initialisation
-      passed in from DFTB+ */
-  typedef struct {
+/**
+   Combined application programming interface and application binary
+   interface semantic version, as supported by this library. Note, the
+   format and is externally defined in the DFTB+ project
 
-    bool initialised;
-    int number;
-    
-  } mystate;
+   @param major Major version, revised on breaking changes
+   @param minor Minor version, revised on extensions
+   @param patch Patch version, revised on invisible changes
 
-
-  /**
-     Combined application programming interface and application binary
-     interface semantic version, as supported by this library. Note,
-     the format and is externally defined in the DFTB+ project
-
-     @param major Major version, revised on breaking changes
-     @param minor Minor version, revised on extensions
-     @param patch Patch version, revised on invisible changes
-
-  */
-  void dftbp_model_apbi(int* major, int* minor, int* patch){
-    *major = 0;
-    *minor = 1;
-    *patch = 0;
-  }
-
-
-  /**
-     Declare capabilities of this model to DFTB+ via the external model API.
-
-     @param modelname null terminated string for name of this model
-     @param capabilities structure with capabilities of the model
-
-   */
-  void dftbp_provided_with(char* modelname, typeof (mycapabilities) *capabilities){
-
-    // Name of external model
-    sprintf(modelname, "Huckel toy model");
-
-    // flags for capabilities of this specific model
-    *capabilities = (mycapabilities) {
-      .hamiltonian = true, .overlap = false, .energy = false, .selfconsistent = false,
-      .spinchannels = 0, .atomsubset = false, .mpi = false
-    };
-
-    return;
-  }
-
-  /**
-     Set up this model, read some settings from DFTB+ over it's
-     external model API and initialise it's data structure for
-     handling via DFTB+.
-
-     @param nspecies number of chemical species/types present
-     @param species array of null terminated strings labelling chemical species
-     @param cutoffs array of cutoffs for distance over which atoms of
-     each species have hamiltonian interactions
-     @param state internal state and data of the model, not checked in
-     DFTB+, just passed around
-     @param message return message, in event of routine failure (return != 0)
-
-     @return 0 on successful return, non-zero if there is an error
-     message to check
-
-   */
-  int initialise_model_for_dftbp(int* nspecies, char* species[], double cutoffs[], typeof (mystate) *state,
-                                 char* message) {
-
-    FILE *input;
-    int i;
-
-    /* Open input file for some constants for this model, assuming
-       it's in the runtime directory */
-    input=fopen("input.dat", "r");
-    if (!input) {
-      sprintf(message, "Library error opening input file.\n");
-      return -1;
-    }
-
-    // This specific model is only for H and C atoms, so will throw an error otherwise
-    for (i=0;i<*nspecies;i++) {
-      if (strcmp(species[i], "C") != 0 && strcmp(species[i], "H") != 0) {
-	sprintf(message, "Toy library only knows about C and H atoms, not %s.\n", species[i]);
-	return -2;
-      }
-
-      if (strcmp(species[i], "C") == 0) {
-        cutoffs[i] = 2.35;
-      }
-      if (strcmp(species[i], "H") == 0) {
-        cutoffs[i] = 1.35;
-      }
-
-    }
-
-    *state = (mystate) {.initialised = true, .number = 6};
-
-    // blank return message if nothing happening
-    sprintf(message, "\n");
-    return 0;
-
-  }
-
-
-  /**
-     Update this model, using geometric and other information from
-     DFTB+ over it's external model API.
-
-     @param state internal state and data of the model, this is not
-     checke by DFTB+, just passed around by it
-
-     @param message return message, in event of routine failure
-     (return != 0)
-
-     @return 0 on successful return, non-zero if there is an error
-     message to check
-
-   */
-  int update_model_for_dftbp(typeof (mystate) *state, char* message) {
-
-    printf("\nInternal check, Model is initialised? ");
-    printf((*state).initialised ? "true\n" : "false\n");
-    printf("Internal model state cargo %d\n", (*state).number);
-
-    // blank return message if nothing happening
-    sprintf(message, "\n");
-    return 0;
-
-  }
-
-  /**
-     Clean up after this model, freeing any memory in the mystate type
-
-     @param state internal state and data of the model. This is not
-     checke by DFTB+, just passed around by it, so we need to remove
-     any allocated memory here.
-
-     @param message return message, in event of routine failure
-     (return != 0)
-
-     @return 0 on successful return, non-zero if there is an error
-     message to check
-
-  */
-  int cleanup_model_for_dftbp(typeof (mystate) *state, char* message) {
-
-    printf("\nInternal check, Model is initialised? ");
-    printf((*state).initialised ? "true\n" : "false\n");
-
-    // blank return message if nothing happening
-    sprintf(message, "\n");
-    return 0;
-
-  }
-
-#ifdef __cplusplus
+*/
+void dftbp_model_apbi(int* major, int* minor, int* patch){
+  *major = 0;
+  *minor = 1;
+  *patch = 0;
 }
-#endif
+
+
+/**
+   Declare capabilities of this model to DFTB+ via the external model
+   API.
+
+   @param modelname null terminated string for name of this model
+   @param capabilities structure with capabilities of the model
+
+*/
+void dftbp_provided_with(char* modelname, typeof (mycapabilities) *capabilities){
+
+  // Name of external model
+  sprintf(modelname, "Huckel toy model");
+
+  // flags for capabilities of this specific model
+  *capabilities = (mycapabilities) {
+    .hamiltonian = true, .overlap = false, .energy = false, .selfconsistent = false,
+    .spinchannels = 0, .atomsubset = false, .mpi = false
+  };
+
+  return;
+}
+
+/**
+   Set up this model, read some settings from DFTB+ over it's external
+   model API and initialise it's data structure for handling via
+   DFTB+.
+
+   @param nspecies number of chemical species/types present
+   @param species array of null terminated strings labelling chemical species
+   @param cutoffs array of cutoffs for distance over which atoms of
+   each species have hamiltonian interactions
+   @param state internal state and data of the model, not checked in
+   DFTB+, just passed around
+   @param message return message, in event of routine failure (return != 0)
+
+   @return 0 on successful return, non-zero if there is an error
+   message to check
+
+*/
+int initialise_model_for_dftbp(int* nspecies, char* species[], double cutoffs[], typeof (mystate) *state,
+                               char* message) {
+
+  FILE *input;
+  int i;
+
+  /* Open input file for some constants for this model, assuming it's
+     in the runtime directory */
+  input=fopen("input.dat", "r");
+  if (!input) {
+    sprintf(message, "Library error opening input file.\n");
+    return -1;
+  }
+
+  // This specific model is only for H and C atoms, so will throw an error otherwise
+  for (i=0;i<*nspecies;i++) {
+    if (strcmp(species[i], "C") != 0 && strcmp(species[i], "H") != 0) {
+      sprintf(message, "Toy library only knows about C and H atoms, not %s.\n", species[i]);
+      return -2;
+    }
+
+    if (strcmp(species[i], "C") == 0) {
+      cutoffs[i] = 2.35;
+    }
+    if (strcmp(species[i], "H") == 0) {
+      cutoffs[i] = 1.35;
+    }
+
+  }
+
+  *state = (mystate) {.initialised = true, .number = 6};
+
+  // blank return message if nothing happening
+  sprintf(message, "\n");
+  return 0;
+
+}
+
+
+/**
+   Update this model, using geometric and other information from DFTB+
+   over it's external model API.
+
+   @param state internal state and data of the model, this is not
+   checke by DFTB+, just passed around by it
+
+   @param message return message, in event of routine failure
+   (return != 0)
+
+   @return 0 on successful return, non-zero if there is an error
+   message to check
+
+*/
+int update_model_for_dftbp(typeof (mystate) *state, char* message) {
+
+  printf("\nInternal check, Model is initialised? ");
+  printf((*state).initialised ? "true\n" : "false\n");
+  printf("Internal model state cargo %d\n", (*state).number);
+
+  // blank return message if nothing happening
+  sprintf(message, "\n");
+  return 0;
+
+}
+
+/**
+   Clean up after this model, freeing any memory in the mystate type
+
+   @param state internal state and data of the model. This is not
+   checke by DFTB+, just passed around by it, so we need to remove any
+   allocated memory here.
+
+   @param message return message, in event of routine failure
+   (return != 0)
+
+   @return 0 on successful return, non-zero if there is an error
+   message to check
+
+*/
+int cleanup_model_for_dftbp(typeof (mystate) *state, char* message) {
+
+  printf("\nInternal check, Model is initialised? ");
+  printf((*state).initialised ? "true\n" : "false\n");
+
+  // blank return message if nothing happening
+  sprintf(message, "\n");
+  return 0;
+
+}
